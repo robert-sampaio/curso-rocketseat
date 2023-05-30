cb989badf6fcc3d1668c455d613b1ccd07e16fca
Robert Sampaio <robertfqsampaio@gmail.com>
2023-05-30 17:21:18 -0300

    add promise02

--- js-assync-promisses/promise02.js
+++ js-assync-promisses/promise02.js
@@ -1,5 +1,11 @@
-import axios from "axios";
+const axios = require("axios");
 
 axios
-  .get("https://api.github.com/user/robert-sampaio")
-  .then((res) => console.log(res.data));
+  .get("https://api.github.com/users/robert-sampaio")
+  .then((response) => {
+    const user = response.data;
+
+    return axios.get(user.repos_url);
+  })
+  .then((repos) => console.log(repos.data))
+  .catch((error) => console.log(error));
